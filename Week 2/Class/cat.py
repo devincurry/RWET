9ddc2d99b020a2befee5cd8af1ff329bc4df07e1
Devin Curry <ddc310@nyu.edu>
2015-02-10 23:30:34 -0500

    Revert "all current files added"
    This reverts commit 351bdea2a469cee118b315b6d755ce770119316c.

--- Week 2/Class/cat.py
+++
@@ -1,6 +0,0 @@
-# our very first program!
-import sys
-for line in sys.stdin:
-	line = line.strip()
-	print line
-	#print line[:4]
--- Week 2/Class/grep.py
+++
@@ -1,7 +0,0 @@
-# our very first program!
-import sys
-for line in sys.stdin:
-	line = line.strip()
-	if "you" in line:
-		print line
-		#print line.replace("something","something")
